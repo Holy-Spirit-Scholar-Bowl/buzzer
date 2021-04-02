98ffc5d3131a0c068a8f6312e47b9d82cd808027
AlexHalbesleben <alexhalb@outlook.com>
2021-04-02 08:43:24 -0500

    Remove PWA
    Attempt 1 at removing PWA support. It's not working right and kind of useless, so I'm taking it out.

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,3 @@
 module.exports = {
   publicPath: "/hssb/",
-
-  pwa: {
-    name: 'HS Scholar Bowl',
-    themeColor: '#8B0000',
-    msTileColor: '#8B0000',
-    manifestOptions: {
-      // eslint-disable-next-line @typescript-eslint/camelcase
-      background_color: '#8B0000'
-    },
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      swSrc: 'src/service-worker.js',
-    }
-  }
 }
